5c02bc0ff2825c35d59af6c8c6f86a691db0d66f
maxpetlyak <maxpetlyak@gmail.com>
2023-01-15 17:11:57 +0200

    hw1 js

---
+++ script.js
@@ -0,0 +1,20 @@
+// task-1
+
+let a = "Maksym";
+let b = a;
+console.log(b);
+
+// task 2
+
+let days = prompt("Напишіть кількість днів, від 1 до 10");
+let daysSecond = days * (24 * 60 * 60);
+if (days > 10) {
+  prompt("Помилка");
+} else {
+  console.log(daysSecond);
+}
+
+// task-3
+
+let info = prompt("Put some text, please");
+console.log(info);
